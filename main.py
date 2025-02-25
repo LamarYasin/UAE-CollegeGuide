19f7209298764f71cac9990f8870bd70e00e9fa9
LamarYasin <lamaryassin2010@gmail.com>
2025-02-25 22:41:38 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 import streamlit as st
 from data.majors import SUBJECTS_TO_MAJORS, MAJOR_DESCRIPTIONS
 from data.universities import UAE_UNIVERSITIES
